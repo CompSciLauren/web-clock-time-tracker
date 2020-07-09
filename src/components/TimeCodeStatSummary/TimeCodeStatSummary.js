4719d3b84587193a4f9d5504171b9f9dd110f574
Lauren Stephenson <compscilauren@gmail.com>
2020-07-09 18:56:12 -0500

    Add statistics page with time code summary (#6)
    * Add statistics page with time code summary

---
+++ src/components/TimeCodeStatSummary/TimeCodeStatSummary.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Card from "terra-card";
+import Heading from "terra-heading";
+import TimeCodeProject from "../ViewTimeCodes/TimeCodeProject";
+import "../../styles/ViewTimeCodes.css";
+
+const TimeCodeStatSummary = () => {
+  return (
+    <Card variant="default" style={{ maxWidth: "40vw", margin: "auto" }}>
+      <div className="my-time-codes-container">
+        <Heading level={1} size="large" weight={700}>
+          This Week's Time Code Summary
+        </Heading>
+        <TimeCodeProject
+          timeCode="19378847"
+          projectTitle="Fancy Project"
+          tag="In-Progress"
+          timeSpent="4.5 hours"
+        />
+        <TimeCodeProject
+          timeCode="39572956"
+          projectTitle="Classified"
+          tag="Not Started"
+          timeSpent="8 hours"
+        />
+        <TimeCodeProject
+          timeCode="82659301"
+          projectTitle="DevOps Magic"
+          tag="In-Progress"
+          timeSpent="6.35 hours"
+        />
+        <TimeCodeProject
+          timeCode="92847634"
+          projectTitle="Wow a project"
+          tag="Completed"
+          timeSpent="17 hours"
+        />
+      </div>
+    </Card>
+  );
+};
+
+export default TimeCodeStatSummary;
