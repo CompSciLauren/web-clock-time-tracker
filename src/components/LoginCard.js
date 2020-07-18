39a31bbec01471d066e8e2a3d844efd447218cba
Lauren Stephenson <compscilauren@gmail.com>
2020-07-18 09:52:13 -0500

    Fix input fields taking too long to type into

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -4,7 +4,7 @@ import InputField from "terra-form-input/lib/InputField";
 import Button from "terra-button/lib/Button";
 import NotificationBanner from "../components/NotificationBanner.js";
 
-const LoginCard = (props) => {
+const LoginCard = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [showRegisterCard, setShowRegisterCard] = useState(false);
@@ -29,130 +29,113 @@ const LoginCard = (props) => {
     console.log("New Password:", newConfirmPassword);
   };
 
-  function RevealLoginCard(props) {
-    if (!props.isVisible) {
-      return null;
-    }
-
-    return (
-      <Card
-        variant="raised"
-        style={{ marginBottom: "20px", paddingBottom: "16px" }}
-      >
-        <NotificationBanner
-          type="error"
-          message="This feature is not fully functional on the website yet."
-        />
-        <main id="main-content" style={{ width: "50%", margin: "auto" }}>
-          <h1>Log in to your account</h1>
-          <form>
-            <InputField
-              label="Username"
-              inputId="inputUsername"
-              type="string"
-              placeholder="myusername"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-
-            <br />
-
-            <InputField
-              label="Password"
-              inputId="inputPassword"
-              type="password"
-              placeholder="mypassword"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-
-            <br />
-
-            <br />
-            <Button variant="action" text="Log in" onClick={handleLogin} />
-            <Button
-              variant="action"
-              text="Register"
-              onClick={handleRegister}
-              style={{ marginLeft: "12px" }}
-            />
-          </form>
-        </main>
-      </Card>
-    );
-  }
-
-  function RevealRegisterCard(props) {
-    if (!props.isVisible) {
-      return null;
-    }
-
-    return (
-      <Card
-        variant="raised"
-        style={{ marginBottom: "20px", paddingBottom: "16px" }}
-      >
-        <NotificationBanner
-          type="error"
-          message="This feature is not fully functional on the website yet."
-        />
-        <main id="main-content" style={{ width: "50%", margin: "auto" }}>
-          <h1>Register a new account</h1>
-          <form>
-            <InputField
-              label="Username"
-              inputId="inputNewUsername"
-              type="string"
-              placeholder="myusername"
-              value={newUsername}
-              onChange={(e) => setNewUsername(e.target.value)}
-            />
-
-            <br />
-
-            <InputField
-              label="Password"
-              inputId="inputPassword"
-              type="password"
-              placeholder="mypassword"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
-
-            <InputField
-              label="Confirm Password"
-              inputId="inputConfirmPassword"
-              type="password"
-              placeholder="mypassword"
-              value={newConfirmPassword}
-              onChange={(e) => setNewConfirmPassword(e.target.value)}
-            />
-
-            <br />
-
-            <br />
-            <Button
-              variant="action"
-              text="Create My Account"
-              onClick={handleCreateMyAccount}
-            />
-            <Button
-              variant="action"
-              text="Back"
-              onClick={handleRegister}
-              style={{ marginLeft: "12px" }}
-            />
-          </form>
-        </main>
-      </Card>
-    );
-  }
-
   return (
-    <div>
-      <RevealLoginCard isVisible={!showRegisterCard} />
-      <RevealRegisterCard isVisible={showRegisterCard} />
-    </div>
+    <>
+      {!showRegisterCard ? (
+        <Card
+          variant="raised"
+          style={{ marginBottom: "20px", paddingBottom: "16px" }}
+        >
+          <NotificationBanner
+            type="error"
+            message="This feature is not fully functional on the website yet."
+          />
+          <main id="main-content" style={{ width: "50%", margin: "auto" }}>
+            <h1>Log in to your account</h1>
+            <form>
+              <InputField
+                label="Username"
+                inputId="inputUsername"
+                type="string"
+                placeholder="myusername"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+
+              <br />
+
+              <InputField
+                label="Password"
+                inputId="inputPassword"
+                type="password"
+                placeholder="mypassword"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <br />
+
+              <br />
+              <Button variant="action" text="Log in" onClick={handleLogin} />
+              <Button
+                variant="action"
+                text="Register"
+                onClick={handleRegister}
+                style={{ marginLeft: "12px" }}
+              />
+            </form>
+          </main>
+        </Card>
+      ) : (
+        <Card
+          variant="raised"
+          style={{ marginBottom: "20px", paddingBottom: "16px" }}
+        >
+          <NotificationBanner
+            type="error"
+            message="This feature is not fully functional on the website yet."
+          />
+          <main id="main-content" style={{ width: "50%", margin: "auto" }}>
+            <h1>Register a new account</h1>
+            <form>
+              <InputField
+                label="Username"
+                inputId="inputNewUsername"
+                type="string"
+                placeholder="myusername"
+                value={newUsername}
+                onChange={(e) => setNewUsername(e.target.value)}
+              />
+
+              <br />
+
+              <InputField
+                label="Password"
+                inputId="inputPassword"
+                type="password"
+                placeholder="mypassword"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+
+              <InputField
+                label="Confirm Password"
+                inputId="inputConfirmPassword"
+                type="password"
+                placeholder="mypassword"
+                value={newConfirmPassword}
+                onChange={(e) => setNewConfirmPassword(e.target.value)}
+              />
+
+              <br />
+
+              <br />
+              <Button
+                variant="action"
+                text="Create My Account"
+                onClick={handleCreateMyAccount}
+              />
+              <Button
+                variant="action"
+                text="Back"
+                onClick={handleRegister}
+                style={{ marginLeft: "12px" }}
+              />
+            </form>
+          </main>
+        </Card>
+      )}
+    </>
   );
 };
 
