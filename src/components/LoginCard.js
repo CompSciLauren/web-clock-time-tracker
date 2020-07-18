d51261ec3867e170f98fb4d78fa41977f6e17c29
Lauren Stephenson <compscilauren@gmail.com>
2020-07-18 00:29:54 -0500

    Add onChange handlers

---
+++ src/components/LoginCard.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Card from "terra-card";
+import InputField from "terra-form-input/lib/InputField";
+import Button from "terra-button/lib/Button";
+import NotificationBanner from "../components/NotificationBanner.js";
+
+const LoginCard = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    console.log("Username:", username);
+    console.log("Password:", password);
+  }
+
+  function handleRegister() {
+    console.log("Register button not implemented yet.");
+  }
+
+  return (
+    <div>
+      <Card
+        variant="raised"
+        style={{ marginBottom: "20px", paddingBottom: "16px" }}
+      >
+        <NotificationBanner
+          type="error"
+          message="This feature is not fully functional on the website yet."
+        />
+        <main id="main-content" style={{ width: "50%", margin: "auto" }}>
+          <h1>Log in to your account</h1>
+          {/* <form onSubmit={handleSubmit}> */}
+          <form>
+            <InputField
+              label="Username"
+              inputId="inputUsername"
+              type="string"
+              placeholder="myusername"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <br />
+
+            <InputField
+              label="Password"
+              inputId="inputPassword"
+              type="password"
+              placeholder="mypassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <br />
+
+            <br />
+            <Button variant="action" text="Log in" onClick={handleLogin} />
+            <Button
+              variant="action"
+              text="Register"
+              onClick={handleRegister}
+              style={{ marginLeft: "12px" }}
+            />
+          </form>
+        </main>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginCard;
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,63 +1,7 @@
 import React from "react";
-import Card from "terra-card";
-import InputField from "terra-form-input/lib/InputField";
-import Button from "terra-button/lib/Button";
-import NotificationBanner from "../../components/NotificationBanner.js";
-
+import LoginCard from "../../components/LoginCard.js";
 function login() {
-  return (
-    <div>
-      <Card
-        variant="raised"
-        style={{ marginBottom: "20px", paddingBottom: "16px" }}
-      >
-        <NotificationBanner
-          type="error"
-          message="This feature is not fully functional on the website yet."
-        />
-        <main id="main-content" style={{ width: "50%", margin: "auto" }}>
-          <h1>Log in to your account</h1>
-          {/* <form onSubmit={handleSubmit}> */}
-          <form>
-            <InputField
-              label="Username"
-              inputId="inputUsername"
-              type="string"
-              placeholder="myusername"
-              // value={totalHoursNeeded}
-              // onChange={(e) => setTotalHoursNeeded(e.target.value)}
-            />
-
-            <br />
-
-            <InputField
-              label="Password"
-              inputId="inputPassword"
-              type="password"
-              placeholder="mypassword"
-              // value={hoursWorked}
-              // onChange={(e) => setHoursWorked(e.target.value)}
-            />
-
-            <br />
-
-            <br />
-            <Button
-              variant="action"
-              text="Log in"
-              // onClick={handleRemindMe}
-            />
-            <Button
-              variant="action"
-              text="Register"
-              // onClick={handleRemindMe}
-              style={{ marginLeft: "12px" }}
-            />
-          </form>
-        </main>
-      </Card>
-    </div>
-  );
+  return <LoginCard />;
 }
 
 export default login;
